0ce8d44264896c63b07e37a89fd09a58f81bcf42
Trueszky <andras.nemeth93@gmail.com>
2018-05-17 17:40:19 +0200

    almostperfectrainbowbox

---
+++ week-02/day-04/CenterSquare/CenterSquare/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CenterSquare
+{
+    // Draw a green 10x10 square to the canvas' center.
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            CenterSquareMethod(foxDraw, 400, 225,200);
+
+
+        }
+        public static void CenterSquareMethod(FoxDraw foxDraw, double x, double y,double hw)
+        {            
+            for (int i = 0; i < 3; i++)
+            {               
+                foxDraw.DrawRectangle(x-(hw/2), y-(hw/2), hw, hw);
+                foxDraw.FillColor(Colors.Beige);
+            }
+        }
+    }
+}
---
+++ week-02/day-04/RainBowBoxes/RainBowBoxes/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RainBowBoxes
+{
+    // Create a square drawing function that takes 2 parameters:
+    // The square size, and the fill color,
+    // and draws a square of that size and color to the center of the canvas.
+    // Create a loop that fills the canvas with rainbow colored squares,
+    // all squares should be centered, but growing in size
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            BoxesOfRainbow(foxDraw, 400, 222);
+        }
+        public static void BoxesOfRainbow(FoxDraw foxDraw, double x,byte y)
+        {
+            
+            for(double i = 0; i <= x; i+=25)
+            {
+                
+                foxDraw.FillColor(Color.FromRgb(y+=55,y+=35,y+=25));
+                foxDraw.StrokeColor(Color.FromRgb(y+=45,y-=255,y+=15));
+                foxDraw.DrawRectangle(400 - ((x-i) / 2), 225 - ((x-i)/ 2), x-(i), x-(i));
+                
+            }
+        }
+    }
+}

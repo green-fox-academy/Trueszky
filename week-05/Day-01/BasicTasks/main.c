f6e37fe25265aaf10fb373ec2bdb1b32a978ccec
Trueszky <andras.nemeth93@gmail.com>
2018-06-18 15:34:59 +0200

    feat(BasicTasks) Precent

--- week-05/Day-01/BasicTasks/main.c
+++ week-05/Day-01/BasicTasks/main.c
@@ -54,6 +54,19 @@ int main()
         // Print the reminder of 22 divided by 13
         printf("%d \n\n",var=a%b);
 
+        // An average Green Fox attendee codes 6 hours daily
+        // The semester is 17 weeks long
+        float precent;
+        // Print how many hours is spent with coding in a semester by an attendee,
+        // if the attendee only codes on workdays.
+        var = 5*17*6;
+        printf("%d \n", var);
+        // Print the percentage of the coding hours in the semester if the average
+        // work hours weekly is 52
+        precent=5*6*100/52;
+        printf("%.2f %%\n\n",precent);
+
+
 
     return 0;
 }

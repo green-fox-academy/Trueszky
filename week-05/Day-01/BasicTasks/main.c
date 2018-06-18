28eb02f8e7f0ff1f5eae7af1f7e7813f0735156b
Trueszky <andras.nemeth93@gmail.com>
2018-06-18 11:59:57 +0200

    feat(BasicTasks) Introdution

--- week-05/Day-01/BasicTasks/main.c
+++ week-05/Day-01/BasicTasks/main.c
@@ -22,5 +22,22 @@ int main()
     // Hello, Joe!
     printf("Hello Joe!\n\n");
 
+      // Write a program that prints a few details to the terminal window about you
+        // It should print each detail to a new line.
+        //  - Your name
+        char name[]="Andreas von Germany";
+        //  - Your age
+        int age = 27;
+        //  - Your height in meters (as a decimal fraction)
+        float height = 1.82;
+        //
+        //  Example output:
+        //  John Doe
+        //  31
+        //  1.87
+        printf("%s\n", name);
+        printf("%d\n", age);
+        printf("%.2f\n\n", height);
+
     return 0;
 }

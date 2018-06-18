569ea6efa7b4e64372e2b80a8c9a9fd831813267
Trueszky <andras.nemeth93@gmail.com>
2018-06-18 15:52:21 +0200

    core(Cuboid) CuboidCalculator

---
+++ week-05/Day-01/Cuboid/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // Write a program that stores 3 sides of a cuboid as variables (doubles)
+    // The program should write the surface area and volume of the cuboid like:
+    //
+    // Surface Area: 600
+    // Volume: 1000
+    double x;
+    double y;
+    double z;
+    double var;
+
+    printf("The surface area is %.2f \n",var=x*y*2+x*z*2+z*y*2);
+    printf("The volume is %.2f mnegyzet\n",var=x*y*z);
+}

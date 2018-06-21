767777724e94e7e49a52b60ad2849e70be45212e
Trueszky <andras.nemeth93@gmail.com>
2018-06-21 16:33:54 +0200

    relocation

---
+++ week-05/Day-04/Parsing/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    // create a function which takes a char array as a parameter,
+    // and splits a string to words by space
+    // solve the problem with the proper string.h function
+
+    printf("Type in a sentence:\n\n");
+    char string[256];
+
+    printf("Hello world!\n");
+    return 0;
+}

23380d9054a9b5c93db3ab8bac02ca284ffadc95
Trueszky <andras.nemeth93@gmail.com>
2018-06-21 17:16:22 +0200

    core(Parsing) BasicTasksHalfOfIt

--- week-05/Day-04/Parsing/main.c
+++ week-05/Day-04/Parsing/main.c
@@ -8,8 +8,16 @@ int main()
     // and splits a string to words by space
     // solve the problem with the proper string.h function
 
-    printf("Type in a sentence:\n\n");
     char string[256];
+    FILE *path = fopen("Test.txt", "w");
+    printf("Type in a sentence:\n\n");
+    gets(string);
+    fprintf(path,string);
+    for (char *p = strtok(string, " "); p != NULL; p = strtok(NULL, " "))
+    {
+        puts(p);
+        fputs(p,path);
+    }
 
     printf("Hello world!\n");
     return 0;
